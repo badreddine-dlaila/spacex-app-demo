7ec5e0cc8599d24ed7b91491b9d29725c9f251da
LaureenMartina <L.martina@protonmail.com>
2019-06-07 17:51:40 +0200

    add details page for one launch

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,4 +1,8 @@
+import { LaunchService } from './../services/launch.service';
+import { Launch } from './../Models/launch.model';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { NavParams } from '@ionic/angular';
 
 @Component({
   selector: 'app-details',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.page.scss'],
 })
 export class DetailsPage implements OnInit {
+  launch: Launch;
+  observableLaunch: Observable<Launch[]>;
 
-  constructor() { }
+  constructor(private launchService: LaunchService) { }
 
-  ngOnInit() {
+  private getLaunchId(): string {
+    return window.location.pathname.split("/").pop();
   }
 
+  ngOnInit() {
+    //console.log(this.navParams.get("launchId"));
+    const launchId = this.getLaunchId();
+    console.log(launchId);
+
+    this.launchService.getOneLaunch(launchId).subscribe(result => {
+      this.launch = result;
+    })
+
+    setTimeout(() => {
+      this.observableLaunch = this.launchService.getLaunches();
+    }, 2000);
+  }
 }
