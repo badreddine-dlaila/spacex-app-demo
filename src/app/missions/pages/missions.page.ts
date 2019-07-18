45b2a6539b9f791d76249be518aedae0faf06312
Tristan Luong <tristan.luong@gmail.com>
2019-07-18 16:55:35 +0200

    Add missions page

--- src/app/missions/pages/missions.page.ts
+++ src/app/missions/pages/missions.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import {Observable} from 'rxjs';
+import { Mission } from '../models/mission.model';
+import { MissionsService } from '../services/missions.service';
 
 @Component({
   selector: 'app-missions',
@@ -8,13 +11,23 @@ import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 })
 export class MissionsPage implements OnInit {
   missionId = 'F3364BF';
+  randomNumber$ = 0;
+  missions$: Observable<Mission[]>;
 
-  constructor(private inAppBrowser: InAppBrowser) { }
+  constructor(private inAppBrowser: InAppBrowser, private missionService: MissionsService) { }
 
   ngOnInit() {
+    this.randomNumber$ = this.getRandomIntInclusive(0, 2);
+    this.missions$ = this.missionService.getAllMissions();
   }
 
   openLink(link: string) {
     this.inAppBrowser.create(link, '_system');
+  }
+
+  getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min +1)) + min;
   }
 }
